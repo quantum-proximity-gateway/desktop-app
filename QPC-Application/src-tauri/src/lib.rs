65b9b98476c48648d629164d5b2d6bbbc442fc0c
Marwan <yassinimarwan6@gmail.com>
2024-12-27 00:24:08 +0100

    playing around with ollama API on rust

--- QPC-Application/src-tauri/src/lib.rs
+++ QPC-Application/src-tauri/src/lib.rs
@@ -1,3 +1,22 @@
+use ollama_rs::{generation::completion::{
+        request::GenerationRequest, GenerationContext
+    },
+    Ollama
+    
+};
+
+#[tauri::command]
+async fn list_models() -> Result<Vec<String>, String> {
+    let ollama = Ollama::default();
+    match ollama.list_local_models().await {
+        Ok(res) => {
+            let models: Vec<String> = res.into_iter().map(|model| model.name).collect();
+            Ok(models)
+        }
+        Err(e) => Err(format!("Failed to list models: {}", e)),
+    }
+}
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -8,7 +27,7 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, list_models])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- QPC-Application/src/App.tsx
+++ QPC-Application/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 import { Button, Input, Text } from "@chakra-ui/react";
@@ -6,23 +6,36 @@ import { Button, Input, Text } from "@chakra-ui/react";
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
+  const [models, setModels] = useState([]);
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
     setGreetMsg(await invoke("greet", { name }));
   }
+  
+  async function listModels() {
+    setModels(await invoke("list_models"));
+  
+  }
+
+  useEffect(() => { // runs once when the component is mounted
+    listModels();
+  }, []);
 
   return (
     <div className="App">
       <Text fontSize="2xl" textAlign="center" mb={4}>Quantum Proximity Gateway - Preferences AI Agent</Text> 
-      <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100vh" }}>
-        <div style={{ display: "flex", flexDirection: "row", alignItems: "center", gap: "10px" }}>
-          <Input placeholder="Enter your name" value={name} onChange={(e) => setName(e.target.value)} />
-          <Button onClick={greet}>Greet</Button>
+      <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100vh", gap: "20px" }}>
+        <Text>Avaliable models:</Text>
+        <div style={{ marginTop: "20px" }}>
+          {models.map((model, index) => (
+            <div key={index} style={{ marginBottom: "10px" }}>
+              <Button>{model}</Button>
+            </div>
+          ))}
         </div>
-        <Text>{greetMsg}</Text>
       </div>
-    </div>
+  </div>
 
   );
 }
